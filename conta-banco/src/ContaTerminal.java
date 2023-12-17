1999a23050284fc129fca802c1ffdf831b2878bf
Johnes Sena <johnes.sena.pessoal@hotmail.com>
2023-12-17 03:41:19 -0400

    Projeto finalizado!

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -5,16 +5,22 @@ public class ContaTerminal {
         
         double saldo = 237.48; //definido um valor padrão, pois este não pode ser indicado pelo usuário!
         
-        //entrada de dados do usuário para criação da conta:
+        // TODO
+        // Para entrada de dados do usuário, implementar:
+        // formatação para "nomeCliente": iniciar cada nome com letra maiúscula;
+        // limitar a quantidade de dígitos de "numeroConta";
+        // limitar a quantidade de dígitos de "numeroAgencia" e definir padrão com "-";
+        
+        // entrada de dados do usuário para criação da conta:
         Scanner inputScanner = new Scanner(System.in);
             System.out.println("Por favor, me diga o seu nome e sobrenome: ");
-                String nomeCliente = inputScanner.next();
+                String nomeCliente = inputScanner.nextLine();
             System.out.println("Agora, me forneça o número de quatro dígitos da conta (ex:. 1021): ");   
                 int numeroConta = inputScanner.nextInt();
             System.out.println("Perfeito! Para finalizarmos, me informe o número da agência (ex:. 067-8): ");
                 String numeroAgencia = inputScanner.next();
                 
-        //mensagem final com o agradecimento e a impressão dos dados informados pelo usuário:    
+        //mensagem final com o agradecimento e a impressão dos dados informados pelo usuário + fim da classe Scanner:    
             System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " +
                                 numeroAgencia + ", conta " + 
                                 numeroConta + " e seu saldo de R$" + saldo + " já está disponível para saque! ;)");
